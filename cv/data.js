c45405e2fd3d9c25d35b187a25120025e68b0a8f
Vincent Schippefilt <vincent.schippefilt@gfi.be>
2016-08-13 15:57:19 +0200

    knockout tryout

--- cv/data.js
+++ cv/data.js
@@ -1,49 +1,32 @@
 ---
 ---
 var data = 
-[
-{% for employer in site.data.exp %}
-    {
+[{% for employer in site.data.exp %}    {
         "company":
         {
             "name":"{{ employer.Company }}", 
             "activities":
-            [
-{% assign start = employer.StartDate | date: "%Y" %}
-{% assign end = employer.EndDate | default : 'now' | date: "%Y" %}
-{% for year in (start..end) reversed %}
+            [{% assign start = employer.StartDate | date: "%Y" %}{% assign end = employer.EndDate | default : 'now' | date: "%Y" %}{% for year in (start..end) reversed %}
                 {
                     "year":  {{ year }},
                     "projects":[
-{% for project in employer.Projects %}
-    {% assign projectStart  = project.StartDate | date : %Y | round %}
-    {% assign projectEnd  = project.EndDate | date : %Y | round %}
-    {% if projectStart <= year and projectEnd >= year  %}
-        {% assign monthtStart  = project.StartDate | date : "%m" | round %}
-        {% assign monthEnd  = project.EndDate | date : "%m" | round %}
-        {% assign nbMonth =  monthEnd - monthtStart %}
-
+{% for project in employer.Projects %}    
+{% assign projectStart  = project.StartDate | date : %Y | round %}
+{% assign projectEnd  = project.EndDate | date : %Y | round %}
+{% if projectStart <= year and projectEnd >= year  %}
+{% assign monthStart  = project.StartDate | date : "%m" | round %}
+{% assign monthEnd  = project.EndDate | date : "%m" | round %}
+{% assign nbMonth =  monthEnd | minus: monthStart | plus: 12 | modulo: 12 %}
                         {
-                            "NbMonth": {{ nbMonth }},
+                            "NbMonth": 
+                            {% if nbMonth == 0 %} 1 {% else %}{{ nbMonth }}{% endif %},
                             "MonthtStart": "{{ project.StartDate }}" ,
                             "MonthEnd": "{{ project.EndDate }}" ,
                             "NbMonthPercentage" :  {{ 100.0 | divided_by: 12 | times: nbMonth | round }}, 
                             "Project":{{ project | jsonify }}
-                        },
-    {% endif %}
-{% endfor %}
+                        },    {% endif %}{% endfor %}
                     ]
                 },
-{% endfor %}
-
-
-
-            ]
+{% endfor %}]
         }
-    },
-{% endfor %}
-];
-
-function doit() {
-//alert (data[0].company.name);
-}
+    },{% endfor %}];
