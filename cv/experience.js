a2dbd3c2dfd194901ca74218bc3239e65b507522
Vincent Schippefilt <vincent.schippefilt@gfi.be>
2016-08-14 14:02:57 +0200

    update month checking

--- cv/experience.js
+++ cv/experience.js
@@ -24,14 +24,13 @@ function displayProjects(data) {
                     else {
                         if (startDate.getFullYear() == y && startDate.getMonth() <= m)
                             // the project starts the year y
-
                             shouldAddProject = true;
 
                         else if (endDate.getFullYear() == y && endDate.getMonth() >= m)
                             // the project ends the year y
-
                             shouldAddProject = true;
                         else
+                        if (startDate.getFullYear() != y && endDate.getFullYear() != y)
                             // the project neither starts nor ends the year y, so it runs the full year
                             shouldAddProject = true;
                     }
